e9b8f3503d9bcc27c34b653dabda28e8df388ee7
gandis0713 <gandis0713@gmail.com>
2025-10-24 01:02:54 +0900

    modify log pattern

--- common/utils/log.cpp
+++ common/utils/log.cpp
@@ -1,28 +1,33 @@
 #include "log.hpp"
 #include <spdlog/sinks/stdout_color_sinks.h>
 
-namespace medusa {
-namespace utils {
+namespace medusa
+{
+namespace utils
+{
 
 std::shared_ptr<spdlog::logger> Logger::logger_ = nullptr;
 
 void Logger::init(const std::string& name, spdlog::level::level_enum level)
 {
-    if (logger_) {
+    if (logger_)
+    {
         return; // Already initialized
     }
 
     // Create console logger with color
     logger_ = spdlog::stdout_color_mt(name);
     logger_->set_level(level);
-    logger_->set_pattern("[%Y-%m-%d %H:%M:%S.%e] [%n] [%^%l%$] %v");
+    // logger_->set_pattern("[%Y-%m-%d %H:%M:%S.%e] [%n] [%^%l%$] %v");
+    logger_->set_pattern("[%n] [%^%l%$] %v");
 
     logger_->info("Logger initialized: {}", name);
 }
 
 std::shared_ptr<spdlog::logger> Logger::get()
 {
-    if (!logger_) {
+    if (!logger_)
+    {
         init(); // Auto-initialize with defaults if not already done
     }
     return logger_;
@@ -30,7 +35,8 @@ std::shared_ptr<spdlog::logger> Logger::get()
 
 void Logger::set_level(spdlog::level::level_enum level)
 {
-    if (logger_) {
+    if (logger_)
+    {
         logger_->set_level(level);
     }
 }
