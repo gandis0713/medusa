929a0eb80c3c9da4b45d459b36cf912542d9bb29
gandis0713 <gandis0713@gmail.com>
2025-10-27 00:48:17 +0900

    v3dv_device 생성자에서 시스템 정보 로그 제거
    - v3dv_device 생성자에서 sysinfo 관련 코드 제거
    - 시스템 RAM 정보 로그 출력 제거

--- medusa/driver/v3dv_device.cpp
+++ medusa/driver/v3dv_device.cpp
@@ -1,7 +1,5 @@
 #include "v3dv_device.hpp"
 
-#include <sys/sysinfo.h>
-
 #include "utils/log.hpp"
 
 namespace medusa
@@ -10,11 +8,6 @@ namespace medusa
 v3dv_device::v3dv_device()
 {
     LOG_INFO("v3dv_device created");
-
-    struct sysinfo info;
-    sysinfo(&info);
-
-    LOG_INFO("System has {} total RAM and {} free RAM (in bytes)", info.totalram, info.freeram);
 }
 
 v3dv_device::~v3dv_device()
