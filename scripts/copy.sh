e4734e5324bb050a686cb5fae6aadca23dba010d
gandis0713 <gandis0713@gmail.com>
2025-11-16 18:19:51 +0900

    scripts/copy.sh 파일 추가: libmedusa2.so 및 medusa_icd.json 파일 복사 명령어 포함

---
+++ scripts/copy.sh
@@ -0,0 +1,3 @@
+sudo cp /home/changhyun/Desktop/git/github/gandis/medusa/build/arm64-rpi5-ninja/debug/medusa/libmedusa2.so ~/Desktop/libs/
+# sudo cp changhyun_icd.json /usr/share/vulkan/icd.d/
+sudo cp /home/changhyun/Desktop/git/github/gandis/medusa/build/arm64-rpi5-ninja/debug/medusa/medusa_icd.json /etc/vulkan/icd.d/
